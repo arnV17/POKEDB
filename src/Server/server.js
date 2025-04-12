c99facbfbe9130518734667796eb794fcb58d6b9
arnV17 <arnavog@gmail.com>
2025-04-12 12:59:10 +0530

    Update server.js

--- src/Server/server.js
+++ src/Server/server.js
@@ -15,10 +15,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-await mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+await mongoose.connect(process.env.MONGODB_URI);
 
 // Connect to MongoDB
 
